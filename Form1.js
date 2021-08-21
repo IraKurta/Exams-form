f2ae299293777c1674d6af78141ec960c82073f6
IraKurta <49121918+IraKurta@users.noreply.github.com>
2021-08-21 09:37:30 +0300

    Create Form1.js

---
+++ Form1.js
@@ -0,0 +1,20 @@
+function changeBG(){
+	document.body.style.background = "#986060";
+	
+}
+function changeBordOutl(){
+	document.getElementsByTagName('section')[0].style.border = "2px solid black";
+	document.getElementsByTagName('section')[0].style.outline = "2px solid green";
+}
+function changeListStyle(){
+	document.getElementsByTagName('ul')[0].style.listStyle = "circle inside";
+}
+function changeMargPadd(){
+	document.body.style.margin = "20px";
+	document.body.style.padding = "20px";
+}
+function changeColorFont(){
+	document.body.style.color = "blue";
+	document.body.style.fontFamily = "Times New Roman";
+}
+
